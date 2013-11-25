f5552a203fff271002bf452abc960a03d7b7053c
Alexander Duchene <aduche4@tigers.lsu.edu>
2013-11-25 15:20:00 -0600

    Basic test for parcels, still needs work

---
+++ tests/miscellaneous/parcel_stack.cpp
@@ -0,0 +1,39 @@
+//  Copyright (c) 2013 Alexander Duchene
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpxpi/xpi.h>
+#include <hpx/util/lightweight_test.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+XPI_Err XPI_main(size_t nargs, void* args[])
+{
+    XPI_Parcel parcel;
+    HPX_TEST_EQ(XPI_Parcel_create(&parcel), XPI_SUCCESS);
+
+    char* data = new char[2];
+    data[0]='a';
+    data[1]='b';
+    HPX_TEST_EQ(XPI_Parcel_set_env(parcel,2*sizeof(char),data), XPI_SUCCESS);
+
+    HPX_TEST_EQ(XPI_Parcel_push(parcel), XPI_SUCCESS);
+    HPX_TEST_EQ(XPI_Parcel_pop(parcel, XPI_NULL), XPI_SUCCESS);
+
+    return XPI_SUCCESS;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+    int result = 0;
+
+    HPX_TEST_EQ(XPI_init(&argc, &argv, 0), XPI_SUCCESS);
+
+    HPX_TEST_EQ(XPI_run(argc, argv, &result), XPI_SUCCESS);
+    HPX_TEST_EQ(result, XPI_SUCCESS);
+
+    HPX_TEST_EQ(XPI_finalize(), XPI_SUCCESS);
+
+    return hpx::util::report_errors();
+}
