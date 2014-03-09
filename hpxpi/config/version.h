257e0eccbee14601e471f3893751203567edac97
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-03-09 14:53:20 -0500

    Updating version (1.0.310)

--- hpxpi/config/version.h
+++ hpxpi/config/version.h
@@ -1,4 +1,4 @@
-//  Copyright (c) 2013 Hartmut Kaiser
+//  Copyright (c) 2013-2014 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -11,9 +11,9 @@ extern "C" {
 #endif
 
 // This is the version of the document currently implemented by HPXPI
-#define XPI_VERSION_MAJOR   0
-#define XPI_VERSION_MINOR   9
-#define XPI_VERSION_RELEASE 176      // document revision
+#define XPI_VERSION_MAJOR   1
+#define XPI_VERSION_MINOR   0
+#define XPI_VERSION_RELEASE 310      // document revision
 
 #if defined(__cplusplus)
 }
